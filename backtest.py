f0daf727aed4966724be3066b00d2416a39d38e1
Rakshada Renapurkar <98681667+Rakshada1811@users.noreply.github.com>
2025-12-14 15:54:04 +0530

    Add signal tracking to run_backtest function
    Store all signals generated during backtesting.

--- backtest.py
+++ backtest.py
@@ -3,13 +3,16 @@ from code_generator import evaluate_ast
 
 def run_backtest(ast, price_data):
     in_position = False
+    all_signals = []
 
     for i in range(len(price_data["close"])):
         data_slice = {
-            "close": price_data["close"][: i + 1]
+            "close": price_data["close"][: i + 1],
+            "volume": price_data["volume"][: i + 1]
         }
 
         signals = evaluate_ast(ast, data_slice)
+        all_signals.append(signals)
 
         if signals["ENTRY"] and not in_position:
             print(f"BUY at index {i}")
@@ -18,3 +21,5 @@ def run_backtest(ast, price_data):
         elif signals["EXIT"] and in_position:
             print(f"SELL at index {i}")
             in_position = False
+
+    return all_signals
